cff18d5a4d29912ab4b1ccd72b3253e79d09e181
Luan E <luan.erazo@skyrocketlab.space>
2021-09-08 15:37:09 -0500

    first comit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="bg-black p-5">
+  <div class="flex flex-col my-auto w-full h-full">
+<img class="max-h-full lg:max-h-96" width="100%" height="100%" src="~/static/svg/Logo.svg" alt="Logo SkyRocket">
+<h3 class="font-sans-space text-white text-5xl text-center text-">STUDIO</h3>
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+<style>
+body {
+  @apply bg-black;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      black: '#19061B'
+    },
+    extend: {
+      fontFamily: {
+        'sans-space': 'Ubuntu, sans-serif'
+      } 
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
